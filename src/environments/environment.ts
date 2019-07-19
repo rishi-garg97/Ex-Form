22a268f02dfa0e620622861a0b89a4ee642c2ddc
Rishi Garg <rishigargabu@gmail.com>
2019-07-19 22:56:55 +0530

    Revert "hiding config info of firebase"
    This reverts commit 8f40cefb92852e3b0bfafa1772cfbc6358298dd1.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "api key",
-    authDomain: "auth Domain",
-    databaseURL: "url",
-    projectId: "project id",
+    apiKey: "AIzaSyAhthWRtzWHuG_C6xyKhNU3wPD_Ck9PS34",
+    authDomain: "form-35b09.firebaseapp.com",
+    databaseURL: "https://form-35b09.firebaseio.com",
+    projectId: "form-35b09",
     storageBucket: "",
-    messagingSenderId: "messaging sender id",
-    appId: "app id"
+    messagingSenderId: "368882932305",
+    appId: "1:368882932305:web:891e308aaacd6c73"
   }
 };
 
